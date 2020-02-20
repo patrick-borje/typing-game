018b459313dbcab28d6119c725bbbf98dd4e4fc6
Patrick Borje <borje.pb@gmail.com>
2020-02-20 09:44:45 +0800

    changed title

---
+++ build/precache-manifest.b94d77e72485aa3a0800d011b6bce88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95debc2ad59b01bd2ede2da35230981",
+    "url": "/typing-game/index.html"
+  },
+  {
+    "revision": "fbde9930152eb6c94f77",
+    "url": "/typing-game/static/css/main.179785ef.chunk.css"
+  },
+  {
+    "revision": "77c87c1b97943cd71562",
+    "url": "/typing-game/static/js/2.aced891d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/typing-game/static/js/2.aced891d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbde9930152eb6c94f77",
+    "url": "/typing-game/static/js/main.fd5887d8.chunk.js"
+  },
+  {
+    "revision": "0287b35be4d7742b7add",
+    "url": "/typing-game/static/js/runtime-main.3059ad6c.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/style.css";
+import useWordGame from "./hooks/useWordGame";
 
 function App() {
+  const {
+    changeVal,
+    text,
+    timeRemaining,
+    startGame,
+    wordCount,
+    isTrue,
+    textBoxRef
+  } = useWordGame();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How Fast Can You Type?</h1>
+      <textarea
+        onChange={changeVal}
+        value={text}
+        disabled={!isTrue}
+        ref={textBoxRef}
+      />
+      <h4>Time Remaining: {timeRemaining}</h4>
+      {wordCount > 0 ? (
+        <button onClick={startGame} disabled={isTrue}>
+          Play Again
+        </button>
+      ) : (
+        <button onClick={startGame} disabled={isTrue}>
+          Start
+        </button>
+      )}
+      {wordCount ? <h4>Word Count: {wordCount}</h4> : null}
     </div>
   );
 }
